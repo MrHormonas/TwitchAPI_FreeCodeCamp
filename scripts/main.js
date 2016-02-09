3bfeae6d3e18329f828aa0c2199c191cc5164bb5
Luís Melo <luismelo7@gmail.com>
2016-02-09 15:50:51 +0000

    streamers divided by online offline ok

--- scripts/main.js
+++ scripts/main.js
@@ -46,31 +46,33 @@ var getLines = function(opt)
   for(var i = 0; i < twitchers.length; i++)
   {
     $.getJSON('https://api.twitch.tv/kraken/channels/' + twitchers[i] + '?callback=?', function(data) {
-      st = data.status;
+      console.log(data);
       if (opt == 'all')
       {
         str  = "<tr><a href='" + data.url + "'>";
         str += getLogo(data.logo);
-        str += "<td colspan='6' class= 'name'>" + data.display_name + "</td>";
+        str += "<td colspan='4' class= 'name'>" + data.display_name + "</td>";
         if (data.status === null)
-          str += "<td>OFFLINE</td></a></tr>";
+          str += "<td colspan='1'></td><td colspan='1'>OFFLINE</td></a></tr>";
         else
-          str += "<td>ONLINE</td></a></tr>";
+          str += "<td colspan='1'>" +  data.status + "</td><td colspan='1'>ONLINE</td></a></tr>";
+        $('.tw').append(str);
       }
 
-      else if (opt == 'on' && st !== null)
+      else if (opt == 'on' &&  data.status !== null)
       {
         str  = "<tr><a href='" + data.url + "'>";
         str += getLogo(data.logo);
         str += "<td colspan='6' class= 'name'>" + data.display_name + "</td></a></tr>";
+        $('.tw').append(str);
       }
       else if (opt == 'off' && data.status === null)
       {
         str  = "<tr><a href='" + data.url + "'>";
         str += getLogo(data.logo);
         str += "<td colspan='6' class= 'name'>" + data.display_name + "</td></a></tr>";
+        $('.tw').append(str);
       }
-      $('.tw').append(str);
     });
   }
 };
